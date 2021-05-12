9adacc90ed3819c366bb40822e67ef8f007e5c40
taku matsunaga <skyline199306@yahoo.co.jp>
2021-05-12 18:11:39 +0900

    second commit

---
+++ src/components/organisms/layouts/Header.tsx
@@ -0,0 +1,17 @@
+import { Flex, Heading } from "@chakra-ui/layout";
+import { memo, VFC } from "react";
+
+export const Header: VFC = memo(() => {
+  return (
+    <Flex 
+    as="nav" 
+    bg="teal.500" 
+    color="gray.50" 
+    align="center" 
+    justify="space-between"
+    padding={{ base: 3, md: 5 }}
+    >
+      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>ユーザー管理アプリ</Heading>
+    </Flex>
+  )
+});
